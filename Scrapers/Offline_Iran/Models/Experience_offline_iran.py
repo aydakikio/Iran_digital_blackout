26c4055871a96ac32e4faac6ab4e292b144b18f9
Ayda Khoshkhan <kikioayda24@gmail.com>
2026-05-07 18:39:13 +0330

    Revert "Add offline Iran scraper"

--- Scrapers/Offline_Iran/Models/Experience_offline_iran.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-import datetime
-
-@dataclass
-class Experience_Data:
-    sender_name: str = None
-    experience_text: str = None
-    published_date: datetime = None
