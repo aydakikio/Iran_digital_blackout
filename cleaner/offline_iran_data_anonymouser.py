6db0f095056e427ac768c1eb4cbff62e018831d7
aydakikio <kikioayda24@gmail.com>
2026-05-14 04:05:21 +0330

    Add offline iran scraper logs and datas

---
+++ cleaner/offline_iran_data_anonymouser.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('data.csv', encoding='utf-8')
+
+col = 'sender_name'
+
+df[col] = df[col].astype(str).str.split('،', n=1).str[1].str.strip()
+
+df.to_csv('dat_raw.csv', index=False, encoding='utf-8')
