e1f45f861baaebc6dc4b4482716b4cb2776bdb4b
aydakikio <kikioayda24@gmail.com>
2026-07-10 21:22:03 +0330

    Version 10.5

--- Scrapers/Citna/Modules/extractor.py
+++ Scrapers/Citna/Modules/extractor.py
@@ -1,12 +1,39 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup,Tag
+from collections import deque
+import jdatetime
+from datetime import datetime , date
+from zoneinfo import ZoneInfo
 
 class Extractor :
+    @staticmethod
+    def extract_news_link(html_content:BeautifulSoup, pending_news:deque):
+        articles_wrapper:Tag = html_content.find("div",class_='item-list')
+
+        articles:Tag=articles_wrapper.find('ul')
+
+        for article in articles.children:
+
+            if not isinstance(article, Tag):
+                continue
+
+            published_datetime=article.find('div',class_='field--name-node-post-date').get_text(strip=True).replace(' - ','').strip()
+            print(f'raw::: {published_datetime}')
+            print(f'transformed:::: {Extractor.parse_jalali_datetime(published_datetime)}')
+
 
     @staticmethod
-    def extract_news():
+    def extract_news(html_content:BeautifulSoup):
+        print()
 
+
+    @staticmethod
+    def extract_comments():
         print()
 
     @staticmethod
-    def extract_comments(self):
-        print()
+    def parse_jalali_datetime(datetime_string: str) -> datetime:
+        iran_timezone = ZoneInfo("Asia/Tehran")
+        jalali_datetime = jdatetime.datetime.strptime(datetime_string.strip(), "%Y-%m-%d %H:%M").replace(tzinfo=iran_timezone)
+
+        return jalali_datetime.togregorian().astimezone(ZoneInfo("UTC"))
+
--- Scrapers/Citna/citna.py
+++ Scrapers/Citna/citna.py
@@ -1,6 +1,9 @@
 #This scrapes the https://www.citna.ir website
 #This website contains news about Internet
 
+#modules
+from Modules.extractor import Extractor
+
 #Libararies
 from botasaurus.browser import browser, Driver
 from botasaurus_driver.solve_cloudflare_captcha import bypass_if_detected
@@ -14,18 +17,37 @@ current_news_page:int = 0
 pending_news:deque[str] = deque()
 pending_comments:deque[str] = deque()
 
-#url of news page -> https://www.citna.ir/taxonomy/term/56877?q=taxonomy/term/56877&page=0
+"""
+Categories
+Internet
+https://www.citna.ir/taxonomy/term/56877?q=taxonomy/term/56877&page=0
+
+Cyber Security
+https://www.citna.ir/taxonomy/term/58698?page=0
+
+Goverment
+https://www.citna.ir/taxonomy/term/58250?page=0
+
+"""
 
 @browser(cache=False, reuse_driver=True,headless=True)
 def citna_scraper(driver:Driver, data=None) -> int:
     global current_news_page
+    global pending_news
 
     driver.enable_human_mode()
     bypass_if_detected(driver)
 
-    driver.get(f'https://www.citna.ir/taxonomy/term/56877?q=taxonomy/term/56877&page={current_news_page}')
-    print()
+    driver.get(f'https://www.citna.ir/taxonomy/term/58250?page={current_news_page}',bypass_cloudflare=True)
+    driver.long_random_sleep()
+
+    webpage_content: BeautifulSoup = soupify(driver)
+
+    Extractor.extract_news_link(webpage_content,pending_news)
+
     return 0
 
 if __name__ == '__main__':
-    print()
+    #citna_scraper()
+
+    print(Extractor.parse_jalali_date('1405-04-19 12:46'))
