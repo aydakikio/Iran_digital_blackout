2066fac700f43b03bdfe02dc42d923322bc7cdcb
aydakikio <kikioayda24@gmail.com>
2026-07-18 02:54:47 +0330

    Version 10.6

---
+++ Scrapers/Citna/Models/news_data.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+
+
+@dataclass
+class News:
+    url: str = "None"
+    title: str = "None"
+    description:str = "None"
+    body: str = "None"
+    news_code: str = "None"
+    reporter: str = "None"
+    likes:int = 0
+
+    tags: list[str] = field(default_factory=list)
+    published_time: datetime = field(default_factory=datetime.now)
